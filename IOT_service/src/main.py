2c2775cf5525fac27dbdab4e73d596db5f4ed74b
Niroshan-k <lakshanniroshan822@gmail.com>
2026-04-18 23:04:26 +0530

    docker build/up working without erros: backend apis working

--- IOT_service/src/main.py
+++ IOT_service/src/main.py
@@ -12,3 +12,14 @@ app.include_router(routes.router)
 @app.get("/")
 def health_check():
     return {"status": "IoT Ingestion Service is running"}
+
+####### check thisssss
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows React to fetch data
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
